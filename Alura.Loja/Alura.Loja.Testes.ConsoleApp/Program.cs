73225e05564dfb2dc8a1f6d3b3ff9aa241138aaa
Christian Luiz Galafassi <chrisitan.galafassi@gmail.com>
2019-08-13 22:39:39 -0300

    Cliente

--- Alura.Loja/Alura.Loja.Testes.ConsoleApp/Program.cs
+++ Alura.Loja/Alura.Loja.Testes.ConsoleApp/Program.cs
@@ -6,46 +6,22 @@ namespace Alura.Loja.Testes.ConsoleApp
     {
         static void Main(string[] args)
         {
-            var promocao = new Promocao()
+            var cliente = new Cliente()
             {
-                Nome = "Pascoa legal",
-                DataInicio = DateTime.Now,
-                DataFim = DateTime.Now.AddMonths(2)
+                Nome = "Christian",
+                EnderecoEntrega = new Endereco()
+                {
+                    Numero = 12,
+                    Logradouro = "Rua dos invalidos",
+                    Complemento ="302",
+                    Bairo = "Centro",
+                    Cidade = "Lages"
+                }
             };
-
-            Produto p1 = new Produto
-            {
-                Nome = "Pão de trigo",
-                Categoria = "Padaria",
-                PrecoUnitario = 0.4,
-                Unidade = "Unidade"
-            };
-            promocao.IncluirPrdouto(p1);
-            Produto p2 = new Produto
-            {
-                Nome = "Suco de laranja",
-                Categoria = "Bebidas",
-                PrecoUnitario = 8.56,
-                Unidade = "Litros"
-            };
-            promocao.IncluirPrdouto(p2);
-            Produto p3 = new Produto
-            {
-                Nome = "Café",
-                Categoria = "Bebidas",
-                PrecoUnitario = 10,
-                Unidade = "Gramas"
-            };
-            promocao.IncluirPrdouto(p3);
-
-
 
             using (var contexto = new LojaContext())
             {
-                //contexto.Promocoes.Add(promocao);
-                var p = contexto.Promocoes.Find(1);
-
-                contexto.Promocoes.Remove(p);
+                contexto.Clientes.Add(cliente);
 
                 contexto.SaveChanges();
 
