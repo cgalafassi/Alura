0290277a198081778f6980e232e9a7ef7b8b3100
Christian Luiz Galafassi <chrisitan.galafassi@gmail.com>
2019-08-20 21:42:24 -0300

    Template View

--- Angular/alurabank/app/js/controllers/NegociacaoController.js
+++ Angular/alurabank/app/js/controllers/NegociacaoController.js
@@ -1,16 +1,16 @@
 class NegociacaoController {
     constructor() {
         this._negociacoes = new Negociacoes();
+        this._negociacoesView = new NegociacoesView('#negociacoesView');
         this._inputData = document.querySelector('#data');
         this._inputQuantidade = document.querySelector('#quantidade');
         this._inputValor = document.querySelector('#valor');
+        this._negociacoesView.update(this._negociacoes);
     }
     adiciona(event) {
         event.preventDefault();
         const negociacaoNew = new Negociacao(new Date(this._inputData.value.replace(/-/g, ',')), parseInt(this._inputQuantidade.value), parseFloat(this._inputValor.value));
         this._negociacoes.adiciona(negociacaoNew);
-        this._negociacoes.paraArray().forEach(negociacao => {
-            console.log(negociacao);
-        });
+        this._negociacoesView.update(this._negociacoes);
     }
 }
--- Angular/alurabank/app/ts/controllers/NegociacaoController.ts
+++ Angular/alurabank/app/ts/controllers/NegociacaoController.ts
@@ -4,12 +4,14 @@ class NegociacaoController {
     private _inputQuantidade: HTMLInputElement;
     private _inputValor: HTMLInputElement;
     private _negociacoes = new Negociacoes();
+    private _negociacoesView = new NegociacoesView('#negociacoesView');
 
     constructor() {
 
         this._inputData = <HTMLInputElement>document.querySelector('#data');
         this._inputQuantidade = <HTMLInputElement>document.querySelector('#quantidade');
         this._inputValor = <HTMLInputElement>document.querySelector('#valor');
+        this._negociacoesView.update(this._negociacoes);
     }
 
     adiciona(event: Event) {
@@ -24,10 +26,6 @@ class NegociacaoController {
 
         this._negociacoes.adiciona(negociacaoNew);
 
-        this._negociacoes.paraArray().forEach(negociacao =>{
-            console.log(negociacao);
-        });
-
-
+        this._negociacoesView.update(this._negociacoes);
     }
 }
