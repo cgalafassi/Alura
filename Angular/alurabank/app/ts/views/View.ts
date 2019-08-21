ffb831de5d67ec91ff75df88fcc38bea72bec9a5
Christian Luiz Galafassi <chrisitan.galafassi@gmail.com>
2019-08-20 22:06:01 -0300

    <T>

--- Angular/alurabank/app/ts/views/View.ts
+++ Angular/alurabank/app/ts/views/View.ts
@@ -1,9 +1,19 @@
-class View {
+class View<T> {
 
     protected _elemento: Element;
 
     constructor(seletor: string) {
 
         this._elemento = document.querySelector(seletor);
+    }
+
+    update(model: T) {
+
+        this._elemento.innerHTML = this.template(model);
+    }
+
+    template(model: T): string {
+
+        return `<p class="alert alert-info">${model}</p>`;
     }
 }
