19340209baa2e33851cba0a9e05da3bb7dc450ce
Christian Luiz Galafassi <chrisitan.galafassi@gmail.com>
2019-08-20 22:52:55 -0300

    namespace

--- Angular/alurabank/app/ts/views/MensagemView.ts
+++ Angular/alurabank/app/ts/views/MensagemView.ts
@@ -1,7 +1,11 @@
-class MensagemView extends View<string> {
-
-    template(model: string): string {
-
-        return `<p class="alert alert-info">${model}</p>`;
+namespace Views {
+    
+    export class MensagemView extends View<string> {
+    
+        template(model: string): string {
+    
+            return `<p class="alert alert-info">${model}</p>`;
+        }
     }
+    
 }
