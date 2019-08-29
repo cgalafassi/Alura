092978c77f94aceb9c0108128a5e4a6f19bc16b4
Christian Luiz Galafassi <chrisitan.galafassi@gmail.com>
2019-08-28 22:48:10 -0300

    Request inicial

---
+++ Alura.ListaLeitura/Alura.ListaLeitura.App/Startup.cs
@@ -0,0 +1,40 @@
+﻿using Alura.ListaLeitura.App.Repositorio;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Alura.ListaLeitura.App
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            app.Run(Router);
+        }
+        public Task Router(HttpContext context)
+        {
+            var _repo = new LivroRepositorioCSV();
+            var caminhosAtendidos = new Dictionary<string, string>
+            {
+                { "/Livros/ParaLer", _repo.ParaLer.ToString() },
+                { "/Livros/Lendo", _repo.Lendo.ToString() },
+                { "/Livros/Lidos", _repo.Lidos.ToString() }
+            };
+
+            if (caminhosAtendidos.ContainsKey(context.Request.Path))
+            {
+                return context.Response
+                    .WriteAsync(caminhosAtendidos[context.Request.Path]);
+            }
+
+            return context.Response.WriteAsync("Caminho inexistente.");
+        }
+
+        public Task LivrosParaLer(HttpContext context)
+        {
+            var _repo = new LivroRepositorioCSV();
+            return context.Response.WriteAsync(_repo.ParaLer.ToString());
+        }
+    }
+}
