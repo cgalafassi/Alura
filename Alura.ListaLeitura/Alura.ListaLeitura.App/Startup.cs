7d53574d1d972318212c6b7b7928081f2785ca24
Christian Luiz Galafassi <chrisitan.galafassi@gmail.com>
2019-09-04 21:22:30 -0300

    Adicionando primeiro .html

--- Alura.ListaLeitura/Alura.ListaLeitura.App/Startup.cs
+++ Alura.ListaLeitura/Alura.ListaLeitura.App/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
 using System;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -44,16 +45,18 @@ namespace Alura.ListaLeitura.App
             return context.Response.WriteAsync("Livro adicionado com sucesso!");
         }
 
+        private string CarregaArquivoHTML(string nomeArquivo)
+        {
+            var nomeCompletoArquivo = $"HTML/{nomeArquivo}.html";
+            using (var arquivo = File.OpenText(nomeCompletoArquivo))
+            {
+                return arquivo.ReadToEnd();
+            }
+        }
+
         private Task ExibeFormulario(HttpContext context)
         {
-            var html = @"
-                    <html>
-                        <form action='Cadastro/Incluir'>
-                            <input name='titulo' />
-                            <input name='autor' />
-                            <button>Gravar</button>
-                        </form>
-                    </html>";
+            var html = CarregaArquivoHTML("formulario");
             return context.Response.WriteAsync(html);
         }
 
